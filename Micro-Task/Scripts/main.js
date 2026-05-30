245db7c0c21787d6cd5d03e62e796b55b787bf4d
Shreya Sarangi <shreyasarangi2006@gmail.com>
2026-05-30 11:31:09 +0530

    Update main.js

--- Micro-Task/Scripts/main.js
+++ Micro-Task/Scripts/main.js
@@ -1,8 +1,8 @@
-let buttons = document.querySelectorAll("button");
+let buttons = document.querySelectorAll("button"); //Stores all buttons as a list of elements in the variable buttons
 let logList = document.querySelector("#log");
-let videoFeed = document.querySelector("#videoFeed");
-let canvas = document.querySelector("#canvas");
-let snapshot = document.querySelector("#snapshot");
+let videoFeed = document.querySelector("#videoFeed"); //Stores the video element in the variable videoFeed
+let canvas = document.querySelector("#canvas");      //Stores the canvas element in the variable canvas
+let snapshot = document.querySelector("#snapshot");  //Stores the img element for snapshot in the variable snapshot
 let stream = null;
 
 buttons.forEach((button) => {
@@ -12,15 +12,15 @@ buttons.forEach((button) => {
         logList.appendChild(newItem);
 
         if (button.id === "startBtn") {
-            navigator.mediaDevices.getUserMedia({ video: true })
-            .then((s) => {
-                stream = s;
+            navigator.mediaDevices.getUserMedia({ video: true }) //Navigator gives info about browser and media
+            .then((s) => {                                       //mediaDevices in one of the properties of navigator that gives access to connected medid devices
+                stream = s;                                      // Request permission from the user to access camera. If granted, the stream is stored in the variable stream
                 videoFeed.srcObject = stream;
             });
         }
 
         if (button.id === "stopBtn" && stream) {
-            stream.getTracks().forEach(track => track.stop());
+            stream.getTracks().forEach(track => track.stop());  //Stops all tracks in the stream to stop camera feed
             videoFeed.srcObject = null;
             stream = null;
         }
